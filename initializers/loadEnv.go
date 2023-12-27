aab09accb55a71d961e651d4e9c61b3afaa49904
mehran mahabadi <mehran.mah44@gmail.com>
2023-12-27 15:44:37 +0330

    add jwt auth controller

--- initializers/loadEnv.go
+++ initializers/loadEnv.go
@@ -2,6 +2,7 @@ package initializers
 
 import (
 	"github.com/spf13/viper"
+	"time"
 )
 
 type Config struct {
@@ -13,6 +14,15 @@ type Config struct {
 	ServerPort     string `mapstructure:"PORT"`
 
 	ClientOrigin string `mapstructure:"CLIENT_ORIGIN"`
+
+	AccessTokenPrivateKey  string        `mapstructure:"ACCESS_TOKEN_PRIVATE_KEY"`
+	AccessTokenPublicKey   string        `mapstructure:"ACCESS_TOKEN_PUBLIC_KEY"`
+	RefreshTokenPrivateKey string        `mapstructure:"REFRESH_TOKEN_PRIVATE_KEY"`
+	RefreshTokenPublicKey  string        `mapstructure:"REFRESH_TOKEN_PUBLIC_KEY"`
+	AccessTokenExpiresIn   time.Duration `mapstructure:"ACCESS_TOKEN_EXPIRED_IN"`
+	RefreshTokenExpiresIn  time.Duration `mapstructure:"REFRESH_TOKEN_EXPIRED_IN"`
+	AccessTokenMaxAge      int           `mapstructure:"ACCESS_TOKEN_MAXAGE"`
+	RefreshTokenMaxAge     int           `mapstructure:"REFRESH_TOKEN_MAXAGE"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
